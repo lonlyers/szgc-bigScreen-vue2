fa3473967c6ac84f9608f8dd23643e13260b5ebc
zhangzhiwei <zw1503637389@163.com>
2023-11-20 15:29:55 +0800

    🎈 perf:配置好代码风格

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,29 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
     <router-view />
   </div>
 </template>
 <script>
-import autofit from "autofit.js";
+import autofit from 'autofit.js'
 
 export default {
   data() {
     return {
-      goss: "1",
-    };
+      goss: '1'
+    }
   },
   mounted() {
     autofit.init(
       {
         dh: 1080,
         dw: 1920,
-        el: "body",
-        resize: true,
+        el: 'body',
+        resize: true
       },
       false
-    ); // You can disable console prompt output
-  },
-};
+    ) // You can disable console prompt output
+  }
+}
 </script>
 <style lang="less">
 #app {
